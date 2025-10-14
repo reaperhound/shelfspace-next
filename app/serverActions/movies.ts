6d41b12d2f8bcf7ab1ddb2a2afa2a0a28c3dbbc9
reaperhound <reaperhound69004@gmail.com>
2025-10-14 12:40:53 +0530

    letterboxd scrape working

--- app/serverActions/movies.ts
+++ app/serverActions/movies.ts
@@ -1,7 +1,69 @@
 "use server";
+import * as cheerio from "cheerio";
 
 export async function getWatchList() {
-  const result = await fetch("https://letterboxd.com/Reaper_Hound/watchlist/");
-  const html = result.text();
-  return html;
+  let watchlist = [];
+  let pageNum = 1;
+  while (true) {
+    const result = await fetch(
+      `https://letterboxd.com/Reaper_Hound/watchlist/page/${pageNum}`
+    );
+    const html = await result.text();
+    const parsedData = parseWithCheerio(html);
+
+    if (parsedData.movies.length === 0) break;
+
+    const moviesWithPosters = await Promise.all(
+      parsedData.movies.map(async (movie) => {
+        const posterData = await getPosters(movie.link);
+        return {
+          ...movie,
+          ...posterData,
+        };
+      })
+    );
+    watchlist.push(...moviesWithPosters);
+    pageNum++;
+  }
+
+  return watchlist;
+}
+
+async function getPosters(link: string) {
+  let posterLink = `https://letterboxd.com${link}poster/std/230/?k=5c13e84e`;
+  const result = await fetch(posterLink);
+  const pstrLink = await result.json();
+  return pstrLink;
+}
+
+function parseWithCheerio(html: string) {
+  const $ = cheerio.load(html);
+
+  const totalEntries = $(".js-watchlist-content").attr("data-num-entries");
+  const movies = [];
+
+  $(".griditem").each((i, elem) => {
+    const reactComponent = $(elem).find('[data-component-class="LazyPoster"]');
+
+    if (reactComponent.length) {
+      movies.push({
+        title: reactComponent.attr("data-item-name"),
+        slug: reactComponent.attr("data-item-slug"),
+        link: reactComponent.attr("data-item-link"),
+        fullLink: `https://letterboxd.com${reactComponent.attr(
+          "data-item-link"
+        )}`,
+        filmId: reactComponent.attr("data-film-id"),
+        posterUrl: reactComponent.attr("data-poster-url"),
+        year: reactComponent.attr("data-item-name"),
+        cleanTitle: reactComponent.attr("data-item-name"),
+      });
+    }
+  });
+
+  return {
+    totalCount: parseInt(totalEntries),
+    movies,
+    moviesOnPage: movies.length,
+  };
 }
