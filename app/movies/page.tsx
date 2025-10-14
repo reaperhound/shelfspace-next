c420eb8353cbca68a325c7bd498a5b955ca0971f
reaperhound <reaperhound69004@gmail.com>
2025-10-14 11:47:22 +0530

    test

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -2,11 +2,11 @@ import { getWatchList } from "../serverActions/movies";
 
 export default async function MovieGrid() {
   const html = await getWatchList();
-  console.log("🚀 ~ MovieGrid ~ html:", html);
   return (
     <div>
       <title>Movies</title>
       <h1>Movies</h1>
+      <p>{html}</p>
     </div>
   );
 }
